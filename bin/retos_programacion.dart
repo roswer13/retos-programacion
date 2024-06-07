324fddfdcf759c51a17aa3171fb2f7998ec69282
Roswer García <roswer13@gmail.com>
2024-06-07 10:38:20 +0200

    first commit

---
+++ bin/retos_programacion.dart
@@ -0,0 +1,5 @@
+import 'package:retos_programacion/retos_programacion.dart' as retos_programacion;
+
+void main(List<String> arguments) {
+  print('Hello world: ${retos_programacion.calculate()}!');
+}
---
+++ test/retos_programacion_test.dart
@@ -0,0 +1,8 @@
+import 'package:retos_programacion/retos_programacion.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
